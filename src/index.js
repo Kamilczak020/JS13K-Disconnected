7da8bb7aec86ead0ff7f2603a19b372c2d90a10a
Kamil Solecki <kamilczak020@gmail.com>
2018-08-23 13:46:18 +0200

    Base structure for testing

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
-let canvas = document.getElementById('canvas');
-let context = canvas.getContext('2d');
+// ############## INFO ###############
+// Canvas is 600x600
+
+// Css required only for hot-reload support
+const css = require('../style.css');
+
+const canvas = document.getElementById('gamearea');
+const context = canvas.getContext('2d');
+
+// #############  STORE  #############
+const store = {
+
+}
+
+// #############  MENU  ##############
+function drawMenu() {
+  canvas.width = 2000;
+  canvas.height = 2000;
+
+  context.fillStyle = '#54b3a4';
+  context.fillRect(0, 0, 2000, 2000);
+
+  context.fillStyle = 'black';
+  context.font = '70px Arial';
+  context.fillText("Multiplayer", 800, 1000)
+}
+
+drawMenu();
