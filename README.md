5f5b1572bd8010de6f275c654f7341f7c7efbaa6
Matei Copot <matei@copot.eu>
2018-08-24 10:33:17 +0200

    moved story text to readme, made headers more readable, removed funny but visually annoying text, made store purpose clearer, made it clearer what we're going to get rid of after the dev phase

--- README.md
+++ README.md
@@ -1,6 +1,43 @@
 # JS13K-Disconnected
 A JS13K Competition Entry Game. Absolute Work-in-progress.
 
+## Story
+You know that feeling, when you are sitting in front of your PC (ok, just for the sake of peace of mind) or console,
+mindlessly clicking around, opening and closing applications while popping in and out of reddit? 
+
+That feeling, when you just can't decide what exactly you want to do / play?
+
+Yeah. Annoying. I know.
+
+But then, out of the corner of your eye, you spot that one game.
+
+Yes. The one game, whose sight instantly rejoices you, as you already know that this is the thing you've been looking for the whole time.
+
+You eagerly click the icon twice. (I know, it's not 1990, y'all don't have icons and use search. I like my icons, m'kay? anyways...)
+
+The loading screen!
+
+As soon as you see the menu, you start the game...
+
+No.. No... NO! NO INTERNET? SERIOUSLY?
+
+The rage pretty much enveloped you right there. But fear not! We are here to save the net. Literally.
+
+The goal of this game is for you to fix the thing that frustrates us the most. You will fix the internet!
+
+## changes after initial dev phase
+
+### html
+no html/head/body, just
+ - meta favicon tag
+ - inline style tag
+ - canvas
+ - possible additional visual html
+ - script for JS
+
+## js
+ - the canvas will be referenced directly by id, no need to define it
+
 ## Why webpack, why seperate stylesheet, why import css into the js? It doesn't make sense!
 All of that has been done solely to ease the development experience by as much as possible.
 If you are like me, you do not like to have to refresh things everytime you make changes to your code.
--- src/index.js
+++ src/index.js
@@ -3,59 +3,37 @@
  * Version: 0.0.2
  * Issued under: MIT LICENSE
  * Created for: JS13K games coding competition, http://js13kgames.com/
- * 
- * Because towc is a VIM madman, we need to give him something to navigate the file with.
- * Introducing - Filemap!
- * 
- * Note: Actual headings in the file are prefixed with an octothorpe "#".
- * ------------------------------------------
- * 
- * GLOBALS
- * 
- * EVENT_HOOKS
- * 
- * GAME_INIT
- * 
- * ------------------------------------------
- * 
- * Storytime!
- * You know that feeling, when you are sitting in front of your PC (ok, just for the sake of peace of mind) or console,
- * mindlessly clicking around, opening and closing applications while popping in and out of reddit? 
- * That feeling, when you just can't decide what exactly you want to do / play?
- * Yeah. Annoying. I know.
- * But then, out of the corner of your eye, you spot that one game.
- * Yes. The one game, whose sight instantly rejoices you, as you already know that this is the thing you've been looking for the whole time.
- * You eagerly click the icon twice. (I know, it's not 1990, y'all don't have icons and use search. I like my icons, m'kay? anyways...)
- * The loading screen!
- * As soon as you see the menu, you start the game...
- * No.. No... NO! NO INTERNET? SERIOUSLY?
- * The rage pretty much enveloped you right there. But fear not! We are here to save the net. Literally.
- * 
- * The goal of this game is for you to fix the thing that frustrates us the most. You will fix the internet!
+ *
+ * # DEV_ONLY
+ * # GLOBALS
+ * # EVENT_HOOKS
+ * # GAME_INIT
+ *
  */
 
-// Css required only for hot-reload support in the development phase.
-const css = require('../style.css');
-
-// #GLOBALS ------------------------------------------
+// # DEV_ONLY
+// hot-reload
+require('../style.css');
+// will be referenced directly by id
 const canvas = document.getElementById('gamearea');
-const context = canvas.getContext('2d');
-const appStore = {
-  canvasHeight: 0,
-  canvasWidth: 0,
-  state: '',
+
+// # GLOBALS
+const ctx = canvas.getContext('2d');
+// game state (not including player)
+const store = {
+  phase: '',
 }
 
-// #EVENT_HOOKS ------------------------------------------
+// # EVENT_HOOKS
 canvas.addEventListener('click', (event) => {
 
 });
 
-// #GAME_INIT ------------------------------------------
+// # GAME_INIT
 // For dummies - those props set the canvas virtual resolution, not the DOM element size!
-appStore.canvasHeight = canvas.height = 2000;
-appStore.canvasWidth = canvas.width = 2000;
-appStore.state = 'menu';
+const h = canvas.height = 600;
+const w = canvas.width = 600;
+store.phase = 'start';
 
-context.fillStyle = '#53e0bd';
-context.fillRect(0, 0, appStore.canvasWidth, appStore.canvasHeight);
+ctx.fillStyle = '#53e0bd';
+ctx.fillRect(0, 0, w, h);
