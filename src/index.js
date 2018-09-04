f00a882665839bf0bb8662e11d891f491fe6031e
Kamil Solecki <kamilczak020@gmail.com>
2018-09-04 16:09:52 +0200

    Dont' judge, just gotta push it as Im leaving work

--- src/index.js
+++ src/index.js
@@ -66,13 +66,45 @@ class Grid {
         cell.render(this.viewport);
       });
     });
+
+    store.player.render();
   }
 }
+const playerAnimations = {
+  standing: {
+    frame: 0,
+    duration: 1,
+    sequence: [0],
+    tick: 0,
+  },
+  walking: {
+    frame: 0,
+    duration: 2,
+    sequence: [14, 0, 28, 0],
+    tick: 0,
+  }
+}
+class Player {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.speed = 4;
+    // 0 - UP, 1 - DOWN, 2 - LEFT, 3 - RIGHT
+    this.orientation = 0;
+    this.activeAnimation = playerAnimations.walking;
+  }
 
+  render() {
+    context.drawImage(sprites, this.activeAnimation.sequence[this.activeAnimation.frame], 91 + 16 * this.orientation, 14, 16, this.x, this.y, 14 * 5, 16 * 5);
+    this.activeAnimation.frame = ((this.activeAnimation.tick / this.activeAnimation.duration) | 0) % this.activeAnimation.sequence.length;
+    this.activeAnimation.tick++;
+  }
+}
 
 const store = {
   lastTime: Date.now(),
   grid: new Grid(25),
+  player: new Player(500, 300),
   game: {
     phase: '',
     tick: 0,
@@ -84,7 +116,7 @@ const store = {
   },
 }
 
-const player = {
+const playerStore = {
   menu: {
     x: 0,
     y: 410,
@@ -124,6 +156,7 @@ const text = {
   menu: [{
     x: 260,
     y: 50,
+    boxWidth: 50,
     duration: 50,
     holdTime: 0,
     elapsed: 0,
@@ -132,6 +165,7 @@ const text = {
   }, {
     x: 345,
     y: 120,
+    boxWidth: 50,
     duration: 0,
     delay: 55,
     elapsed: 0,
@@ -140,6 +174,7 @@ const text = {
   }, {
     x: 260,
     y: 950,
+    boxWidth: 50,
     elapsed: 0,
     delay: 55,
     duration: 0,
@@ -148,6 +183,7 @@ const text = {
   }, {
     x: 420,
     y: 120,
+    boxWidth: 50,
     elapsed: 0,
     duration: 0,
     scale: 2, 
@@ -155,34 +191,58 @@ const text = {
   }
   ],
   tutorial: [{
-      x: 0,
-      y: 0,
+      x: 30,
+      y: 30,
+      boxWidth: 30,
       elapsed: 0,
-      scale: 3,
-      value: 'Welcome to Reconnected!',
+      duration: 50,
+      scale: 2,
+      value: 'Welcome to Reconnected!\n\nThis game is about you running\naround and fixing things...\n\nGood luck!',
     }]
 };
 
 // # EVENT_HOOKS
 document.addEventListener('keyup', (event) => {
-  if (event.keyCode == 32) {
-    if (store.game.phase == 'start') {
+  if (store.game.phase == 'start') {
+    if (event.keyCode == 32) {
       store.transition.active = true;
-      player.menu.stopX = canvasWidth + 200;    
-    }
-  }
-
-  if (store.game.phase == 'tutorial') {
-    if (event.keyCode == 40) {
-      console.log(store.grid.viewport.y);
-      store.grid.viewport.y += 2;
+      playerStore.menu.stopX = canvasWidth + 200;    
     }
   }
 });
 
+document.addEventListener('keydown', (event) => {
+  if (store.game.phase == 'tutorial') {
+    // Left Arrow
+    if (event.keyCode == 37) {
+      store.player.x -= store.player.speed;
+      //store.grid.viewport.x--;
+    }
+    
+    // Up Arrow
+    if (event.keyCode == 38) {
+      store.player.y -= store.player.speed;
+      store.player.orientation = 0;
+      //store.grid.viewport.y--;
+    }
+
+    // Right arrow
+    if (event.keyCode == 39) {
+      store.player.x += store.player.speed;
+      //store.grid.viewport.x++;
+    }
+
+    // Down arrow
+    if (event.keyCode == 40) {
+      store.player.y += store.player.speed;
+      store.player.orientation = 1;
+      //store.grid.viewport.y++;
+    }
+  }
+})
+
 // # GAME_INIT
-// For dummies - those props set the canvas virtual resolution, not the DOM element size.
-store.game.phase = 'start';
+store.game.phase = 'tutorial';
 
 context.fillStyle = '#b8b8b8';
 context.fillRect(0, 0, canvasWidth, canvasHeight);
@@ -212,8 +272,8 @@ function animate() {
       context.fillStyle = '#b8b8b8';
       context.fillRect(0, 0, canvasWidth, canvasHeight);
 
-      if (player.menu.x < player.menu.stopX) {
-        player.menu.x += player.menu.speed;
+      if (playerStore.menu.x < playerStore.menu.stopX) {
+        playerStore.menu.x += playerStore.menu.speed;
       }
       
       // Draw ground
@@ -221,26 +281,26 @@ function animate() {
       context.fillRect(0, 580, canvasWidth, canvasHeight - 580);
 
       // Draw character
-      const playerAnim = player.menu.x < player.menu.stopX ? player.menu.animations.walking : player.menu.animations.standing;
+      const playerAnim = playerStore.menu.x < playerStore.menu.stopX ? playerStore.menu.animations.walking : playerStore.menu.animations.standing;
       context.drawImage(sprites, 
         selectAnimationFrame(playerAnim) * playerAnim.width,
         0,
         playerAnim.width,
         playerAnim.height,
-        player.menu.x, 
-        player.menu.y, 
+        playerStore.menu.x, 
+        playerStore.menu.y, 
         playerAnim.width * playerAnim.scale, 
         playerAnim.height * playerAnim.scale);
 
       // Draw cable
-      for (let i = 0; i < player.menu.x - cable.menu.sprite.width; i+= cable.menu.sprite.width) {
+      for (let i = 0; i < playerStore.menu.x - cable.menu.sprite.width; i+= cable.menu.sprite.width) {
         context.drawImage(sprites, 
           i % cable.menu.sprite.count * 15,
           40,
           cable.menu.sprite.width,
           cable.menu.sprite.height, 
-          player.menu.x - 40 - cable.menu.sprite.width * cable.menu.sprite.scale * i / (cable.menu.sprite.width + 2), 
-          player.menu.y + 104, 
+          playerStore.menu.x - 40 - cable.menu.sprite.width * cable.menu.sprite.scale * i / (cable.menu.sprite.width + 2), 
+          playerStore.menu.y + 104, 
           cable.menu.sprite.width * cable.menu.sprite.scale, 
           cable.menu.sprite.height * cable.menu.sprite.scale);
       }
@@ -261,6 +321,7 @@ function animate() {
 
         if (transitionAlpha == 1) {
           store.transition.tick = 0;
+          store.game.fpsInterval = 20;
           store.game.phase = 'tutorial';
         }
       } else {
@@ -277,6 +338,15 @@ function animate() {
       context.fillRect(0, 0, canvasWidth, canvasHeight);
 
       store.grid.render();
+
+      // Draw tutorial textbox
+      context.strokeStyle = '#0f4666';
+      context.lineWidth = 8;
+      context.fillStyle = '#2b75a0';
+      context.fillRect(20, 20, 400, canvasHeight - 40);
+      context.strokeRect(20, 20, 400, canvasHeight - 40);
+
+      writeText(text.tutorial[0], true);
 
       if (store.transition.active) {
         const transitionAlpha = 1 - 0.05 * store.transition.tick;
@@ -299,23 +369,27 @@ const textColorCanvas = document.createElement('canvas');
 const textColorContext = textColorCanvas.getContext('2d');
 
 function writeText(text, persistent, color = 'black') {
-
   const characterCount = (text.duration) > 0 ? Math.min(text.value.length, (((text.elapsed) / (text.duration / text.value.length)) | 0))
     : text.value.length;
 
+  let lineNumber = 0;  
+  let lastNewline = 0;
   if (text.delay == 0 || !text.delay) {
     if (text.elapsed < text.duration + text.holdTime || persistent) {
       for (let i = 0; i < characterCount; i++) {
-        writeLetter(text.value[i], text.x + i * text.scale * 6, text.y, text.scale, color);
+        if (text.value.charCodeAt(i) == 10) {
+          lineNumber++;
+          lastNewline = i + 1;
+        }
+
+        writeLetter(text.value[i], text.x + (i - lastNewline) * text.scale * 6, text.y + 9 * text.scale * lineNumber, text.scale, color);
       }
   
       text.elapsed++;
     }
   }
 
-
   if (text.delay > 0) {
-    console.log(text.delay);
     text.delay--;
   }
 }
